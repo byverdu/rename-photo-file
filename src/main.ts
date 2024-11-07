788572496046f9e0e8f4d4c42bb657134959abde
Albert Vallverdu <av@owgtm.com>
2024-11-07 23:19:56 +0000

    app implementation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,65 @@
 import { exifReader, renameFile, sendEmail } from '@modules';
+import Watcher from 'watcher';
+import { DEV_WATCHER_FOLDER, ENVS, VALID_IMAGE_EXTENSION } from '@constants';
+import path from 'node:path';
+import process from 'node:process';
+import 'jsr:@std/dotenv/load';
+import { writeFileSync } from 'node:fs';
 
-const filePath = 'src/example-assets/.test_3.HEIC';
+const NODE_ENV = process.env.NODE_ENV ?? ENVS.DEVELOPMENT;
+const WATCHER_FOLDER_PATH = Deno.env.get('WATCHER_FOLDER_PATH');
+const watchFolder = NODE_ENV === ENVS.PRODUCTION
+  ? WATCHER_FOLDER_PATH
+  : DEV_WATCHER_FOLDER;
 
-try {
-  const fileName = await exifReader(filePath, sendEmail);
-  await renameFile(
-    { oldPath: filePath, newPath: `${fileName ?? ''}` },
-    sendEmail,
-  );
-} catch (error) {
-  console.error(error);
-}
+const watcher = new Watcher(watchFolder, { persistent: true });
+
+let processedFiles = 0;
+let totalFiles = 0;
+
+watcher.on('add', (oldPath) => {
+  if (VALID_IMAGE_EXTENSION.test(path.extname(oldPath))) {
+    totalFiles++;
+  }
+});
+
+watcher.on('add', async (oldPath) => {
+  if (VALID_IMAGE_EXTENSION.test(path.extname(oldPath))) {
+    console.log(`New file to process: ${oldPath}`);
+    let hasErrored = false;
+
+    try {
+      const newPath = await exifReader(oldPath, sendEmail) ?? '';
+
+      await renameFile(
+        { oldPath, newPath },
+        sendEmail,
+      );
+    } catch (error) {
+      // TODO: Send email
+      hasErrored = true;
+      console.error(error);
+    } finally {
+      processedFiles++;
+
+      if (!hasErrored && processedFiles === totalFiles) {
+        const formattedDate = new Date().toLocaleDateString('en-US', {
+          day: '2-digit',
+          month: 'short',
+          year: 'numeric',
+        });
+        console.log('formattedDate', formattedDate);
+        writeFileSync(`${watchFolder}/latest.txt`, new Date().toISOString(), {
+          encoding: 'utf-8',
+        });
+        processedFiles = 0;
+        totalFiles = 0;
+      }
+    }
+  }
+});
+
+watcher.on('error', (error) => {
+  // TODO: Send email
+  console.error('Watcher error:', error);
+});
